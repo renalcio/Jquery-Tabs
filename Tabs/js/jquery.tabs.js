9ef986cf9550234b03f4abb05854b5d70b5950f5
Renalcio Carlos <r.carlos@live.com>
2014-04-15 15:07:13 -0300

    V 1.0
    Versão 1.0 realizada

--- Tabs/js/jquery.tabs.js
+++ Tabs/js/jquery.tabs.js
@@ -1,55 +1,62 @@
 /*
-Plugin NOME
-Data: 28/04/2013
+Plugin Tabs
+Data: 14/04/2014
 Autor: Renalcio Carlos
 Email: r.carlos@live.com
-Opções:
--- funcao:
-* kitbar - barra do userkit no topo do site. (add plugin ba-outside-events e jcookie).
-www.fabricadaweb.com.br
+-------------------------------------------
+Para a v2:
+Width, Height, Cookies, onChange, Efeitos.
+-------------------------------------------
 */
 (function($){
-		$.fn.tabs = function(options){
+		$.fn.tab = function(options){
 		// configurações padrão
 		var config = { 
-			efeito: 'nulo',
-			iniciante: 0,
-            xhtml: true,
+			iniciante: 1,           // Aba Inicial
+            xhtml: true,            // Tags XHTML Personalizadas?
+            menu: "menu",           // Tag de conjunto de Menu
+            abasConteudo: "aba",            // Tag de item de Menu
+            conteudo: "conteudo",   // Tag de Conjunto de Conteudo
+            principal: "tabs",      // Tag do conjunto Principal
+            itensMenu: "item",          // Tag de item de Conteudo
 		};
 	options = $.extend(config,options);
 	return this.each(function(){
+	   options.iniciante = parseInt(options.iniciante)-1;
 		if(options.xhtml==true){
-		//Definições de inicio
-        $("item>titulo", this).addClass("tabTitulo");
-		$("item>conteudo", this).addClass("tabConteudo");
-		$("item:first>titulo", this).addClass("tabTituloAtual");
-		$("item>conteudo", this).hide();
-		$("item:first>conteudo", this).addClass("tabConteudoAtual");
-        $("item:first>conteudo", this).show();
-		
-        $("menu>item", this).click(function(){
-            var num = $(this).index();
-			$(this).parent().find("item").removeClass("tabTituloAtual");
-            $(this).addClass("tabTituloAtual");
-			$(this).parent().parent().find("abas").find("conteudo").hide().removeClass("tabConteudoAtual");
-			$(this).parent().parent().find("abas").find("conteudo").eq(num).show().addClass("tabConteudoAtual");
-		});
+            var menuItem = options.menu+">"+ options.itensMenu;
+            var conteudoAba = options.conteudo+">"+ options.abasConteudo;
+            var conteudo = options.conteudo;
+            var menu = options.menu;
+            var tabs = options.principal;
+            var item = options.itensMenu;
+            var aba = options.abasConteudo;
 		}else{
-		var set = $(this).attr("set");
-		var menuSet = $('li.item', this);
-		var itemSet = $('div.item', this);
-		$("ul", this).addClass("set");
-		$("div."+options.itemClass).hide();
-		$("div#"+options.itemClass+"-1").show();
-		$("ul li:first", this).css("border-color", options.corHover);
-		$("ul li", this).click(function(){
-			var num = $(this).attr('n');
-			$("ul[set="+set+"] li").css("border-color", options.corNormal);
-			$(this).css("border-color", options.corHover);
-			$("div[set="+set+"]."+options.itemClass).hide();
-			$("div[set="+set+"]#"+options.itemClass+"-"+num).fadeIn();
-		});	
+            var menuItem = "ul:first>li";
+            var conteudoAba = "div.conteudo>div.aba";
+            var conteudo = "div.conteudo";
+            var menu = "ul:first";
+            var tabs = "tabs";
+            var item = "li.tabTitulo";
+            var aba = "div.aba";
+            $(this).addClass(tabs);
 		}
+        
+        //Definições de inicio
+        $(menuItem, this).removeClass("tabTituloAtual");
+		$(conteudoAba, this).removeClass("tabConteudoAtual");
+        $(menuItem, this).addClass("tabTitulo");
+		$(conteudoAba, this).addClass("tabConteudo").hide();
+		$(menuItem, this).eq(options.iniciante).addClass("tabTituloAtual");
+		$(conteudoAba, this).eq(options.iniciante).addClass("tabConteudoAtual").show();
+        
+        $(menuItem, this).click(function(){
+            var num = $(this).index();
+			$(this).parent().find(item).removeClass("tabTituloAtual");
+            $(this).addClass("tabTituloAtual");
+			$(this).parent().parent().find(conteudo).find(aba).hide().removeClass("tabConteudoAtual");
+			$(this).parent().parent().find(conteudo).find(aba).eq(num).show().addClass("tabConteudoAtual");
+		});
 	});
 	};
 })(jQuery);
